20b1470afbc0859ff3e293b6b4dbecb2ab044dd5
fluxfw <fluxfw@fluxlabs.ch>
2021-08-13 09:29:08 +0200

    log

---
+++ src/Log/Log.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Fluxlabs\FluxRestApi\Log;
+
+use Throwable;
+
+trait Log
+{
+
+    private function log(Throwable $ex) : void
+    {
+        file_put_contents("php://stdout", $ex);
+    }
+}
