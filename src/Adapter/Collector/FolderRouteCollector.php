1fca0cfbaf787bc145cd952b15a49e3cb5626f35
fluxfw <fluxfw@fluxlabs.ch>
2021-11-03 08:55:29 +0100

    Fix

--- src/Adapter/Collector/FolderRouteCollector.php
+++ src/Adapter/Collector/FolderRouteCollector.php
@@ -39,7 +39,7 @@ class FolderRouteCollector implements RouteCollector
                 continue;
             }
 
-            if (str_ends_with($file->getPathName(), "/vendor/autoload.php")) {
+            if (str_ends_with($file->getPathName(), "/autoload.php")) {
                 require_once $file->getPathName();
 
                 continue;
