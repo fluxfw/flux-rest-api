bda61aab08c5d30d2ce6b8a1e91c2de801c44895
fluxfw <fluxfw@fluxlabs.ch>
2021-12-06 12:17:42 +0100

    remove checker

--- src/autoload.php
+++ src/autoload.php
@@ -5,24 +5,6 @@ namespace FluxRestApi;
 require_once __DIR__ . "/../libs/flux-rest-base-api/autoload.php";
 
 use FluxAutoloadApi\Adapter\Autoload\Psr4Autoload;
-use FluxAutoloadApi\Adapter\Checker\PhpExtChecker;
-use FluxAutoloadApi\Adapter\Checker\PhpVersionChecker;
-
-PhpVersionChecker::new(
-    ">=7.4"
-)
-    ->checkAndDie(
-        __NAMESPACE__
-    );
-PhpExtChecker::new(
-    [
-        "curl",
-        "json"
-    ]
-)
-    ->checkAndDie(
-        __NAMESPACE__
-    );
 
 Psr4Autoload::new(
     [
